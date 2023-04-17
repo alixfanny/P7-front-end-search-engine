796988f7f0b07bd6baaefa95cfaae1552b499ab3
alixfanny <alix.fanny.mail@gmail.com>
2023-04-17 17:37:25 +0200

    html css terminé, implatation des recettes

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ async function getRecipes() {
 }
 
 function displayData(recipes) {
-    console.log(recipes)
     const recipesSection = document.querySelector(".container-recette");
 
     recipes.forEach((recipe) => {
@@ -26,47 +25,59 @@ async function init() {
 init();
 
 function recipeFactory(data) {
-    const {id, name, description, ingredients, time, } = data;
+    const {id, name, description, ingredients, time, portrait } = data;
+    const picture = `media/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
-        article.classList.add('recipe');
+        article.classList.add('recipe', 'col-4');
 
         const div = document.createElement('div');
         div.classList.add('content-img', 'card');
 
         const img = document.createElement( 'img' );
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", name);
+        img.setAttribute("id", id);
         img.classList.add("img-recipe");
 
         const divBody = document.createElement('div');
         divBody.classList.add("card-body")
 
         const divTitle = document.createElement('div');
-        divTitle.classList.add('content-title', "card-text");
+        divTitle.classList.add('content-title', "card-text", "justify-content-between", "d-flex");
 
         const h3 = document.createElement( 'h3' );
         h3.textContent = name;
+
+        const divIconTime = document.createElement('div');
+        divIconTime.classList.add('content-icon', 'd-flex');
+
+        const span = document.createElement('span');
+        span.classList.add('fa-solid', 'fa-clock');
 
         const pTime = document.createElement( 'p' );
         pTime.textContent = time + "min";
         pTime.classList.add('time');
 
         const divDescription = document.createElement('div');
-        divDescription.classList.add('content-description', "card-text");
+        divDescription.classList.add('content-description', "card-text", "d-flex");
 
 
         const pIngredients = document.createElement( 'div' );
         ingredients.forEach(function(ingredient){
             const ingredientEl = document.createElement('div');
+            const ingredientRecipe = ingredient.ingredient;
+
             if(ingredient.unit && ingredient.quantity){
-                ingredientEl.textContent = ingredient.ingredient + ": " + ingredient.quantity +" " + ingredient.unit;
+                ingredientEl.innerHTML = '<strong>' + ingredientRecipe + '</strong>' + ": " + ingredient.quantity +" " + ingredient.unit;
             }
             else if(ingredient.quantity){
-                ingredientEl.textContent = ingredient.ingredient + ": " + ingredient.quantity;
+                ingredientEl.innerHTML = '<strong>' + ingredientRecipe + '</strong>' + ": " + ingredient.quantity;
             }
 
             else {
-                ingredientEl.textContent = ingredient.ingredient;
+                ingredientEl.innerHTML = '<strong>' + ingredientRecipe + '</strong>';
             }
             pIngredients.appendChild(ingredientEl);
         })
@@ -77,11 +88,14 @@ function recipeFactory(data) {
         pDescription.classList.add('description');
 
         article.appendChild(div);
-        article.appendChild(divTitle);
-        article.appendChild(divDescription);
+        article.appendChild(divBody)
+        divBody.appendChild(divTitle);
+        divBody.appendChild(divDescription);
         div.appendChild(img);
         divTitle.appendChild(h3);
-        divTitle.appendChild(pTime);
+        divTitle.appendChild(divIconTime);
+        divIconTime.appendChild(span);
+        divIconTime.appendChild(pTime);
         divDescription.appendChild(pIngredients);
         divDescription.appendChild(pDescription);
         return (article);
